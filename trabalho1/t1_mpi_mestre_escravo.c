a6b0b1fcb5cd68e68577af3ceb923a13f85ffa52
Rafael Accetta <rafaelaccetta@id.uff.br>
2025-10-08 19:22:39 -0300

    mestre_escravo

--- trabalho1/t1_mpi_mestre_escravo.c
+++ trabalho1/t1_mpi_mestre_escravo.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include <mpi.h>
 #include <time.h>
 
-#define TAM 1000000
-#define VETOR_SIZE 1000000
+#define TAM 100000
 
 int main(int argc, char *argv[]) {
     int rank, size;
-    int x, vet[TAM];
-    long long soma_local = 0, subtracao_local = 0;
-    double mult_local = 1.0;  
-    long long soma_total, subtracao_total;
-    double mult_total;
+    int x;
+    double soma = 0.0, subtracao = 0.0, mult = 1.0;
+    double soma_local = 0.0, subtracao_local = 0.0, mult_local = 1.0;
+    double vet[TAM];
     double start_time, end_time;
     
     MPI_Init(&argc, &argv);
@@ -19,62 +18,61 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
     start_time = MPI_Wtime();
-    
-    if (rank == 0) {
-        for (x = 0; x < VETOR_SIZE; x++) {
-            vet[x] = (x % 1000) + 1;  
+
+    if(size < 2){
+        if (rank == 0) {
+            printf("O programa deve ser executado com pelo menos 2 processos.\n");
         }
+        MPI_Finalize();
+        exit(0);
     }
     
-    MPI_Bcast(vet, VETOR_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    int elementos_por_processo = VETOR_SIZE / size;
-    int resto = VETOR_SIZE % size;
-    
-    int inicio = 0;
-    int fim = 0;
-    
-    for (int i = 0; i < size; i++) {
-        int elementos = elementos_por_processo + (i < resto ? 1 : 0);
-        if (i == rank) {
+    if (rank == 0) {
+        for (x = 0; x < TAM; x++) {
+            vet[x] = (((x%5)*0.001) + 1.0);
+        }
+        MPI_Bcast(vet, TAM, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        int inicio = 0, fim = 0;
+        int elementos_por_processo = TAM / (size - 1);
+        int resto = TAM % (size - 1);
+
+        for(x=1;x<size;x++){
             inicio = fim;
-            fim = inicio + elementos;
-            break;
+            fim = inicio + elementos_por_processo + (x <= resto ? 1 : 0);
+            MPI_Send(&inicio, 1, MPI_INT, x, 1, MPI_COMM_WORLD);
+            MPI_Send(&fim, 1, MPI_INT, x, 2, MPI_COMM_WORLD);
         }
-        fim += elementos;
-    }
-    
-    for (x = inicio; x < fim; x++) {
-        soma_local += vet[x];
-        subtracao_local -= vet[x];
-        mult_local *= vet[x];
+
+        MPI_Reduce(&soma_local, &soma, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&subtracao_local, &subtracao, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&mult_local, &mult, 1, MPI_DOUBLE, MPI_PROD, 0, MPI_COMM_WORLD);    
+
+        end_time = MPI_Wtime();
+
+        printf("\n=== RESULTADOS PARA %d ELEMENTOS ===\n", TAM);
+        printf("Soma = %f\n", soma);
+        printf("Subtracao = %f\n", subtracao);
+        printf("Multiplicacao = %f\n", mult);
+        printf("Tempo paralelo (%d processos): %f segundos\n", size, end_time - start_time);
+    } else {
+        MPI_Bcast(vet, TAM, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        int inicio, fim;
+        MPI_Recv(&inicio, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&fim, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        printf("i %d f %d \n", inicio, fim);
         
-        if (mult_local > 1e100 || mult_local < 1e-100) {
-            mult_local = 1.0;  
+        for(x=inicio;x<fim;x++){
+            soma_local = soma_local + vet[x];
+            subtracao_local = subtracao_local - vet[x];
+            mult_local = mult_local * vet[x];
         }
-    }
-    
-    MPI_Reduce(&soma_local, &soma_total, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&subtracao_local, &subtracao_total, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    
-    MPI_Reduce(&mult_local, &mult_total, 1, MPI_DOUBLE, MPI_PROD, 0, MPI_COMM_WORLD);
-    
-    end_time = MPI_Wtime();
-    
-    if (rank == 0) {
-        double tempo_sequencial_estimado = 0.008410; // informação de tempo para calculo de metricas
-        double tempo_paralelo = end_time - start_time;
-        double speed_up = tempo_sequencial_estimado / tempo_paralelo;
-        double eficiencia = (speed_up / size) * 100;
-        
-        printf("=== VERSÃO MESTRE/ESCRAVO - %d PROCESSOS ===\n", size);
-        printf("Soma = %lld\n", soma_total);
-        printf("Subtracao = %lld\n", subtracao_total);
-        printf("Multiplicacao = %e\n", mult_total);
-        printf("Tempo paralelo: %f segundos\n", tempo_paralelo);
-        printf("Speed Up: %.2f\n", speed_up);
-        printf("Eficiencia: %.1f%%\n", eficiencia);
-        printf("Tempo sequencial estimado: %f segundos\n", tempo_sequencial_estimado);
+
+        MPI_Reduce(&soma_local, &soma, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&subtracao_local, &subtracao, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&mult_local, &mult, 1, MPI_DOUBLE, MPI_PROD, 0, MPI_COMM_WORLD);
     }
     
     MPI_Finalize();
