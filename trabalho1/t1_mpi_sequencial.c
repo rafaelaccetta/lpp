700d4ca70cf1508b1598d51d48b5c9e0d3a36945
Rafael Accetta <rafaelaccetta@id.uff.br>
2025-10-08 10:10:32 -0300

    sequencial

--- trabalho1/t1_mpi_sequencial.c
+++ trabalho1/t1_mpi_sequencial.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #include <time.h>
-#include <math.h>
 
-#define VECTOR_SIZE 1000000
+#define TAM 100000
 
 int main() {
     int x;
-    long long soma = 0, subtracao = 0;
-    double log_mult = 0.0;  
-    int vet[VECTOR_SIZE];
-    double start_time, end_time;
-    
-    start_time = clock();
-    
-    for (x = 0; x < VECTOR_SIZE; x++) {
-        vet[x] = (x % 1000) + 1;  
+    double soma = 0, subtracao = 0, mult = 1.0;  
+    double vet[TAM];
+    clock_t start_time, end_time;
+        
+    for (x=0;x<TAM;x++) {
+        vet[x] = (((x%5)*0.001) + 1.0);
     }
-    
-    for (x = 0; x < VECTOR_SIZE; x++) {
-        soma += vet[x];
-        subtracao -= vet[x];
-        log_mult += log(vet[x]);  
+
+    start_time = clock();
+
+    for (x=0;x<TAM;x++) {
+        soma = soma + vet[x];
+        subtracao = subtracao - vet[x];
+        mult = mult * vet[x];
     }
     
     end_time = clock();
-    
-    double mult = exp(log_mult);  
-    
-    printf("=== SEQUENCIAL COM %d ELEMENTOS ===\n", VECTOR_SIZE);
-    printf("Soma = %lld\n", soma);
-    printf("Subtracao = %lld\n", subtracao);
-    printf("Multiplicacao = %e\n", mult);
-    printf("Log10 da Multiplicacao = %f\n", log_mult / log(10));  
-    printf("Tempo sequencial: %f segundos\n", (end_time - start_time) / CLOCKS_PER_SEC);
+        
+    printf("=== SEQUENCIAL COM %d ELEMENTOS ===\n", TAM);
+    printf("Soma = %f\n", soma);
+    printf("Subtracao = %f\n", subtracao);
+    printf("Multiplicacao = %f\n", mult);
+    printf("Tempo sequencial: %f segundos\n", (double) (end_time - start_time) / CLOCKS_PER_SEC);
     
     return 0;
 }
