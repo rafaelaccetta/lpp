e5e1e32d77500170daf114f01a47c597529930b4
Rafael Accetta <rafaelaccetta@id.uff.br>
2025-10-08 11:30:37 -0300

    4 procs

--- trabalho1/t1_mpi_4proc.c
+++ trabalho1/t1_mpi_4proc.c
@@ -3,63 +3,71 @@
 #include <mpi.h>
 #include <time.h>
 
-#define VETOR_SIZE 1000000
+#define TAM 100000
 
 int main(int argc, char *argv[]) {
     int rank, size;
-    int *vet;
-    long long soma = 0, subtracao = 0;
+    int x, op;
+    double soma = 0.0, subtracao = 0.0, mult = 1.0;
+    double vet[TAM];
     double start_time, end_time;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
-    vet = (int*)malloc(VETOR_SIZE * sizeof(int));
-    
+        
     start_time = MPI_Wtime();
-    
-    for (int i = 0; i < VETOR_SIZE; i++) {
-        vet[i] = (i % 1000) + 1; 
+
+    if (size != 4) {
+        if (rank == 0) {
+            printf("O programa deve ser executado apenas com 4 processos.\n");
+        }
+        MPI_Finalize();
+        exit(0);
     }
-    
-    int elementos_por_processo = VETOR_SIZE / size;
-    int inicio = rank * elementos_por_processo;
-    int fim = (rank == size - 1) ? VETOR_SIZE : inicio + elementos_por_processo;
-    
-    long long soma_local = 0, subtracao_local = 0;
-    
-    printf("Processo %d: processando elementos %d a %d\n", rank, inicio, fim-1);
-    
-    for (int i = inicio; i < fim; i++) {
-        soma_local += vet[i];
-        subtracao_local -= vet[i];
-    }
-    
-    long long soma_total, subtracao_total;
-    
-    MPI_Reduce(&soma_local, &soma_total, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&subtracao_local, &subtracao_total, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-    
-    end_time = MPI_Wtime();
     
     if (rank == 0) {
-        printf("\n=== RESULTADOS PARA %d ELEMENTOS ===\n", VETOR_SIZE);
-        printf("Soma = %lld\n", soma_total);
-        printf("Subtracao = %lld\n", subtracao_total);
-        printf("Multiplicacao = OMITIDA (overflow para %d elementos)\n", VETOR_SIZE);
+        for (x = 0; x < TAM; x++) {
+            vet[x] = (((x%5)*0.001) + 1.0);
+        }
+
+        for(x=1;x<4;x++){
+            MPI_Send(vet, TAM, MPI_DOUBLE, x, 1, MPI_COMM_WORLD);
+            op = x;
+            MPI_Send(&op, 1, MPI_INT, x, 2, MPI_COMM_WORLD);
+        }
+
+        MPI_Recv(&soma, 1, MPI_DOUBLE, 1, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&subtracao, 2, MPI_DOUBLE, 2, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&mult, 1, MPI_DOUBLE, 3, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        end_time = MPI_Wtime();
+
+        printf("\n=== RESULTADOS PARA %d ELEMENTOS ===\n", TAM);
+        printf("Soma = %f\n", soma);
+        printf("Subtracao = %f\n", subtracao);
+        printf("Multiplicacao = %f\n", mult);
         printf("Tempo paralelo (%d processos): %f segundos\n", size, end_time - start_time);
-        
-        
-        double tempo_sequencial_estimado = 0.008410; // Calculo atuomático das nétricas
-        double speed_up = tempo_sequencial_estimado / (end_time - start_time);
-        double eficiencia = (speed_up / size) * 100;
-        printf("\n--------------------- Métricas ---------------------\n\n");
-        printf("Speed Up estimado: %.2f\n", speed_up);
-        printf("Eficiencia estimada: %.1f%%\n", eficiencia);
+       
+    } else {
+        MPI_Recv(vet, TAM, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&op, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        if (op == 1){
+            for(x=0;x<TAM;x++)
+                soma = soma + vet[x];
+            MPI_Send(&soma, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+        } else if (op == 2) {
+            for(x=0;x<TAM;x++)
+                subtracao = subtracao - vet[x];
+            MPI_Send(&subtracao, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+        } else {
+            for(x=0;x<TAM;x++)
+                mult = mult * vet[x];
+            MPI_Send(&mult, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+        }
     }
     
-    free(vet);
     MPI_Finalize();
     return 0;
 }
